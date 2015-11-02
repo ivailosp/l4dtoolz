360e1ce2a55f15db178ccb3e3c267e19538f5eff
TyUser <enter20@mail.ru>
2015-11-02 18:48:11 +0500

    Update signature.cpp

--- signature.cpp
+++ signature.cpp
@@ -121,9 +121,9 @@ static int callback(struct dl_phdr_info *info, size_t size, void *data)
 int find_base(const char* name, struct base_addr_t *base_addr)
 {
 #ifdef WIN32
-	HANDLE hModuleSnap = INVALID_HANDLE_VALUE;
+/*	HANDLE hModuleSnap = INVALID_HANDLE_VALUE; */
 	MODULEENTRY32 modent;
-	hModuleSnap = CreateToolhelp32Snapshot(TH32CS_SNAPALL, 0);
+	HANDLE hModuleSnap = CreateToolhelp32Snapshot(TH32CS_SNAPALL, 0);
 	if(hModuleSnap == INVALID_HANDLE_VALUE) {
 		return 0;
 	}
